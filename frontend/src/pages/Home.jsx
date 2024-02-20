259e6ef69f75bb21c678fed40c22a1f8376f9ec7
Aswin246 <mashwinarjun@gmail.com>
2024-02-20 07:12:17 +0530

    Frontend in progress

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,10 +6,10 @@ import { SubHeading } from "../components/SubHeading";
 const Home = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <div className="text-center">
+      <div className="flex justify-center items-center h-screen bg-green-200">
+        <div className="flex flex-col justify-center items-center text-center bg-white w-auto rounded-lg p-8">
           <Heading label="TaskFlow" />
-          <SubHeading label="Simplify Your Workflow with TaskFlow " />
+          <SubHeading label="Simplify Your Workflow with TaskFlow" />
         </div>
       </div>
     </>
