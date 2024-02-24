0b55e969ba3a5aa07117f870873f396e217b7fff
Aswin246 <mashwinarjun@gmail.com>
2024-02-24 22:30:14 +0530

    frontend

--- backend/routes/middleware.js
+++ backend/routes/middleware.js
@@ -11,20 +11,26 @@ const authMiddleware = (req, res, next) => {
   const auth = req.headers.authorization;
 
   if (!auth || !auth.startsWith("Bearer")) {
-    res.json(400).json({
+    return res.status(400).json({
       msg: "Invalid auth token format",
     });
-    return;
   }
+
   const token = auth.split(" ")[1];
-  const decoded = jwt.verify(token, JWT);
-  if (!decoded) {
-    res.json(400).json({
+  try {
+    const decoded = jwt.verify(token, JWT);
+    if (!decoded) {
+      return res.status(400).json({
+        msg: "Invalid auth token",
+      });
+    }
+    req.id = decoded.id;
+    next();
+  } catch (error) {
+    return res.status(400).json({
       msg: "Invalid auth token",
     });
   }
-  req.id = decoded.id;
-  next();
 };
 
 module.exports = authMiddleware;
